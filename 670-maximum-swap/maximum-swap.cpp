8580786ba91addb540dd0316d9c25ecda42e7447
Shivansh Mishra <113025664+shivansh29817@users.noreply.github.com>
2024-10-17 14:25:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (12.29%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-        string nums = to_string(num);
-        string nums2 = nums;
-        
-        sort(nums2.rbegin(), nums2.rend());
-        
-        unordered_map<char, int> mp;
-        for (int i = 0; i < nums.size(); i++) {
-            mp[nums[i]] = i;
+        unordered_map<char,int>mp;
+        string nums=to_string(num);
+        string nums2=nums;
+
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]=i;
         }
-        
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != nums2[i]) {
-                swap(nums[i], nums[mp[nums2[i]]]);
-                break; 
+
+        sort(nums2.rbegin(),nums2.rend());
+        int count=0;
+        int i=0;
+
+        while(count<1 && i<nums.size()){
+            if(nums2[i]==nums[i]) i++;
+            else{
+                swap(nums[mp[nums2[i]]],nums[i]);
+                count=1;
             }
         }
-        
-        return stoi(nums);
+
+        return stoi(nums) ;
+
     }
 };
