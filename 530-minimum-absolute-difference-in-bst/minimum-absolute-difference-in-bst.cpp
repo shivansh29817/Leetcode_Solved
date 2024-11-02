9ccbc5f4313afec04a02f19744eced2d17a34327
Shivansh Mishra <113025664+shivansh29817@users.noreply.github.com>
2024-11-02 22:54:40 +0530

    Time: 0 ms (100.00%) | Memory: 24.1 MB (80.91%) - LeetSync

---
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    void dfs(TreeNode* root, int &ans, int &pre) {
+        if(!root) return;
+        
+        dfs(root -> left, ans, pre);
+        ans = min(ans, root -> val - pre);
+        pre = root -> val;
+        dfs(root -> right, ans, pre); 
+    }
+public:
+    int getMinimumDifference(TreeNode* root) {
+        int ans = 1e9, pre = -1e9;
+        dfs(root, ans, pre);
+        return ans;
+    }
+}; 
