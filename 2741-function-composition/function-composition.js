985e82d7d28a199f8b20bc8a7198661f6f1aec1c
Shivansh Mishra <113025664+shivansh29817@users.noreply.github.com>
2024-11-26 22:48:29 +0530

    Time: 65 ms (49.72%) | Memory: 51.9 MB (5.56%) - LeetSync

--- 2741-function-composition/function-composition.js
+++ 2741-function-composition/function-composition.js
@@ -3,17 +3,15 @@
  * @return {Function}
  */
 var compose = function(functions) {
-    if (functions.length === 0) {
-     return function(x) { return x; };
-    } else {
-    return function(x) {
-      var result = functions[functions.length - 1](x);
-      for (var i = functions.length - 2; i >= 0; i--) {
-        result = functions[i](result);
-      }
-      return result;
-    };
+ if (functions.length === 0) {
+    return function(x) { return x; };
   }
+
+  return functions.reduceRight(function(prevFn, nextFn) {
+    return function(x) {
+      return nextFn(prevFn(x));
+    };
+  });
 };
 
 /**
