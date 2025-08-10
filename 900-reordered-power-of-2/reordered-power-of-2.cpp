3ff1c46f3a71c1df8839043a535fc12cf3858daa
Shivansh Mishra <113025664+shivansh29817@users.noreply.github.com>
2025-08-10 14:40:52 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (51.03%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -4,6 +4,7 @@ public:
         string s = to_string(n);
         sort(s.begin(), s.end()); 
 
+
         for (int i = 0; i < 31; i++) {
             int power = 1 << i;
             string p = to_string(power);
