cac9af0bdc369b40c8925aad6f001eeffae5e532
Shivansh Mishra <113025664+shivansh29817@users.noreply.github.com>
2025-08-30 20:53:27 +0530

    Time: 19 ms (6.32%) | Memory: 25.9 MB (7.64%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        const int num = 9;
-        // Initialize boolean arrays to track digits in each row, column, and subgrid
-        bool row[num][num] = {false};
-        bool col[num][num] = {false};
-        bool subGrid[num][num] = {false};
-        
-        // Iterate through each cell in the Sudoku board
-        for(int r = 0; r < num; ++r){
-            for(int c = 0; c < num; ++c){
-                // Skip empty cells
-                if(board[r][c] == '.')
-                    continue; // if not a number, proceed to the next cell
-                
-                // Convert character digit to numerical index
-                int idx = board[r][c] - '0' - 1; // char to num idx
-                // Determine the subgrid number based on the current cell's position
-                int gridNum = (r/3) * 3 + (c/3);
-                
-                // Check if the number already exists in the corresponding row, column, or subgrid
-                if(row[r][idx] || col[c][idx] || subGrid[gridNum][idx]){
-                    return false; // if duplicate number found, the Sudoku board is invalid
-                }
-                
-                // Update the boolean arrays for the current row, column, and subgrid with the current digit
-                row[r][idx] = true;
-                col[c][idx] = true;
-                subGrid[gridNum][idx] = true;
+        unordered_set<string> st;
+        for(int i= 0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] == '.') continue;
+                string row = string(1, board[i][j]) + " Row " + to_string(i);
+                string col = string(1, board[i][j]) + " Col " + to_string(j);
+                string box = string(1, board[i][j]) + " Box " + to_string(i/3) + " " + to_string(j/3);
+
+                if(st.find(row) != st.end() || st.find(col) != st.end() || st.find(box) != st.end() )
+                    return false;
+                st.insert(row);
+                st.insert(col);
+                st.insert(box);
             }
         }
-        return true; // if no duplicates found, the Sudoku board is valid
+        return true;
     }
 };
